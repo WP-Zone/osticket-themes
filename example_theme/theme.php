28ab2e615ecfef680998b5359e83cd035f7ed060
Jonathan Hall <jonathan@aspengrovestudios.com>
2024-03-15 15:00:55 -0700

    Add support for theme settings and dynamic CSS

--- example_theme/theme.php
+++ example_theme/theme.php
@@ -1,5 +1,5 @@
 <?php
-// This file was modified by Jonathan Hall on 2024-03-11
+// This file was modified by Jonathan Hall on 2024-03-15
 
 class ExampleTheme extends OsTicketTheme {
 	
@@ -21,6 +21,36 @@ class ExampleTheme extends OsTicketTheme {
 	
 	function getMinimumLogoAspectRatio() {
 		return -1;	
+	}
+	
+	function getThemeSettingsFields() {
+		return [
+			'fontFamily' => new ChoiceField([
+				'label' => 'Font Family',
+				'hint' => '',
+				'choices' => [
+					'Arial' => 'Arial',
+					'Open Sans' => 'Open Sans',
+					'Times New Roman' => 'Times New Roman'
+				],
+				'default' => 'Arial'
+			]),
+			'primaryColor' => new ColorChoiceField([
+				'label' => 'Primary Color',
+				'hint' => '',
+				'default' => ''
+			]),
+			'secondaryColor' => new ColorChoiceField([
+				'label' => 'Secondary Color',
+				'hint' => '',
+				'default' => ''
+			]),
+			'logoWidth' => new TextboxField([
+				'label' => 'Logo Width',
+				'hint' => '',
+				'default' => ''
+			])
+		];
 	}
 	
 	
--- themes.php
+++ themes.php
@@ -1,5 +1,5 @@
 <?php
-// This file was modified by Jonathan Hall on 2024-02-22
+// This file was modified by Jonathan Hall on 2024-03-15
 
 require_once(INCLUDE_DIR . 'class.plugin.php');
 require_once(INCLUDE_DIR . 'class.thread.php');
@@ -9,10 +9,12 @@ require_once(INCLUDE_DIR . 'class.osticket.php');
 require_once(__DIR__ . '/includes/class.theme.php');
 
 class WPZThemesPlugin extends Plugin {
+	public static $instance;
 	private $activeTheme = null;
 	public $config_class = 'WPZThemesPluginConfig';
 
     function bootstrap() {
+		self::$instance = $this;
 		$activeTheme = $this->getConfig($this->getInstances()->first())->get('wpz-theme');
 		if ($activeTheme) {
 			try {
@@ -25,18 +27,66 @@ class WPZThemesPlugin extends Plugin {
 	function isMultiInstance() {
 		return false;
 	}
+	
+	function getActiveTheme() {
+		return $this->activeTheme;
+	}
 }
 
 class WPZThemesPluginConfig extends PluginConfig {
 	function getOptions() {
-		return [
-			'wpz-theme' => new ChoiceField([
-				'label' => 'Theme',
-				'hint' => '',
-				'choices' => array_merge(['' => 'Default'], OsTicketTheme::getThemes()),
-				'default' => ''
-			])
-		];
+		if (!empty(WPZThemesPlugin::$instance)) {
+			$activeTheme = WPZThemesPlugin::$instance->getActiveTheme();
+			if (!empty($activeTheme)) {
+				$themeSettingsFields = $activeTheme->getThemeSettingsFields();
+			}
+		}
+		
+		if (!empty($themeSettingsFields)) {
+			$_themeSettingsFields = $themeSettingsFields;
+			$themeSettingsFields = [];
+			$themeSettingPrefix = 'wpz-theme-'.substr(md5($activeTheme->getId()), 0, 9).'-';
+			foreach ($_themeSettingsFields as $settingId => $setting) {
+				$themeSettingsFields[$themeSettingPrefix.$settingId] = $setting;
+			}
+		}
+		
+		
+		return array_merge(
+			[
+				'wpz-theme' => new ChoiceField([
+					'label' => 'Theme',
+					'hint' => '',
+					'choices' => array_merge(['' => 'Default'], OsTicketTheme::getThemes()),
+					'default' => ''
+				])
+			],
+			empty($themeSettingsFields) ? [] : [
+				'wpz-theme-separator' => new SectionBreakField([
+					'label' => $activeTheme->getName().' Settings'
+				])
+			],
+			empty($themeSettingsFields) ? [] : $themeSettingsFields
+		);
+	}
+	
+	function store(SimpleForm $form = null, &$errors=array()) {
+		if (!empty(WPZThemesPlugin::$instance)) {
+			$activeTheme = WPZThemesPlugin::$instance->getActiveTheme();
+		}
+		
+		$result = parent::store($form, $errors);
+		
+		if ($result && !empty($activeTheme)) {
+			try {
+				$activeTheme->buildAllDynamicCss();
+			} catch (Exception $ex) {
+				$result = false;
+			}
+		}
+		
+		return $result;
+		
 	}
 }
 
